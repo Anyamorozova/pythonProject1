46585589974cb4a8598ed8aad7ef699463d343b0
Anyamorozova <44360379+Anyamorozova@users.noreply.github.com>
2021-05-31 16:48:16 +0300

    Create les4_6.py

---
+++ les4_6.py
@@ -0,0 +1,15 @@
+from itertools import cycle
+from itertools import count
+ 
+for el in count(10):
+    if el > 20:
+        break
+    print(el)
+
+my_list = ["test1","test2","test3"]
+count_ = 0
+for el in cycle(my_list):
+    if count_ > 10:
+        break
+    print(el)
+    count_ += 1
