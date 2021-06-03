8bea2268fd51cae52d0d86bea87f6bbde2cc44e3
Anyamorozova <44360379+Anyamorozova@users.noreply.github.com>
2021-06-03 09:29:56 +0300

    Create les5_3.py

---
+++ les5_3.py
@@ -0,0 +1,21 @@
+file = open("my_file.txt", "r")
+content = file.read()
+file.close()
+
+content = content.split("\n")
+list_worker, sum_wage = [], 0
+count_worker = len(content)
+
+for el in content:
+  list_worker.append(el.split())
+  
+print("Сотрудники, имеющие оклад менее 20 тысяч рублей:")
+
+for el in list_worker:
+  if int(el[1]) < 20000:
+    print(el[0])
+  sum_wage += int(el[1])
+  
+average_wage = sum_wage/count_worker
+
+print(f"Средняя величина дохода сотрудников: {average_wage}")
