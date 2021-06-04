d643bd46cdf611d063d617ea01d2bc8d1c2626af
Anyamorozova <44360379+Anyamorozova@users.noreply.github.com>
2021-06-04 16:07:05 +0300

    Create les6_1.py

---
+++ les6_1.py
@@ -0,0 +1,19 @@
+import time
+
+class TrafficLight:
+    __color = ""
+
+    def running(self):
+        self.__color = "Red"
+        print(self.__color)
+        time.sleep(7)
+        self.__color = "Yellow"
+        print(self.__color)
+        time.sleep(2)
+        self.__color = "Green"
+        print(self.__color)
+        time.sleep(2)
+        return "the end"
+        
+trafficLight = TrafficLight()
+print(trafficLight.running())
